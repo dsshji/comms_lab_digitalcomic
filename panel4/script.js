a099d93bdd8ff716967e137e403cb3e50850bf31
Prakrati Mamtani <prakrati7124@gmail.com>
2026-03-22 18:41:05 +0530

    panel 5 added

--- panel4/script.js
+++ panel4/script.js
@@ -2,11 +2,11 @@ let box = document.getElementById('boxInteraction');
 let boxSound = new Audio("boxSound.m4a")
 
 //triggers fucntiuons when user clicks the box
-function nextPanel(){
-    window.location.href = "panel5/index.html" //FIX THIS AND ADD REAL LINK TO PANEL 5
+function nextPanel() {
+    window.location.href = "../panel5/index.html" //FIX THIS AND ADD REAL LINK TO PANEL 5
 }
 
-function boxSoundEffect(){
+function boxSoundEffect() {
     boxSound.play();
     setTimeout(nextPanel, 500) //waits 0.5 secs then runs the function that goes to the next panel
 }
---
+++ panel5/script.js
@@ -0,0 +1,216 @@
+AOS.init();
+
+const dropZone = document.querySelector(".drop-zone");
+const finalImage = document.querySelector(".final-image");
+const heroSection = document.querySelector(".hero");
+const whiteSection = document.querySelector(".white-section");
+
+
+const images = [
+    "../panel5/assets/puzzle/piece1.svg",
+    "../panel5/assets/puzzle/piece2.svg",
+    "../panel5/assets/puzzle/piece3.svg",
+    "../panel5/assets/puzzle/piece4.svg"
+];
+
+let activePiece = null;
+let offsetX = 0;
+let offsetY = 0;
+let placedCorrectly = 0;
+
+/* CREATE PIECES */
+images.forEach((src, index) => {
+
+    const piece = document.createElement("img");
+    piece.src = src;
+    piece.classList.add("piece");
+    piece.dataset.index = index;
+    piece.dataset.placed = "false";
+
+    /* Random place puzzle pieces */
+    let left, top;
+    left = Math.random() * 70 + 10;
+    top = Math.random() * 70 + 10;
+    piece.style.left = left + "%";
+    piece.style.top = top + "%";
+
+
+    if (piece.dataset.index === "1" || piece.dataset.index === "2") {
+        piece.style.width = "8%";
+    } else {
+        piece.style.width = "10%";
+    }
+
+    document.body.appendChild(piece);
+
+    piece.addEventListener("click", (e) => {
+
+        /* If nothing is active → pick up */
+        if (!activePiece && piece.dataset.placed === "false") {
+            activePiece = piece;
+            offsetX = e.offsetX;
+            offsetY = e.offsetY;
+            piece.style.zIndex = 1000;
+        }
+
+        /* If clicking same piece again → drop */
+        else if (activePiece === piece) {
+            const clickX = e.clientX;
+            const clickY = e.clientY;
+            releasePiece(activePiece, clickX, clickY);
+        }
+
+        e.stopPropagation();
+    });
+});
+
+/* Move with cursor */
+document.addEventListener("mousemove", (e) => {
+    if (!activePiece) return;
+
+    activePiece.style.left = (e.clientX - offsetX) + "px";
+    activePiece.style.top = (e.clientY - offsetY) + "px";
+});
+
+/* Clicking anywhere else drops piece */
+document.addEventListener("click", (event) => {
+    if (activePiece) {
+        const clickX = event.clientX;
+        const clickY = event.clientY;
+        releasePiece(activePiece, clickX, clickY);
+    }
+});
+
+function releasePiece(piece, clickX, clickY) {
+    piece.style.transition = "all 0.3s ease";
+    checkSnap(piece, clickX, clickY);
+    activePiece = null;
+    console.log(`Released piece ${piece.dataset.index} at (${clickX}, ${clickY})`);
+}
+
+function checkSnap(piece, clickX, clickY) {
+
+    const dropRect = dropZone.getBoundingClientRect();
+    const index = parseInt(piece.dataset.index);
+
+    if (index == 0) {
+        if (
+            dropRect.left <= clickX &&
+            clickX <= dropRect.left + dropRect.width * 0.5 &&
+            dropRect.top <= clickY &&
+            clickY <= dropRect.top + dropRect.height * 0.5
+        ) {
+            piece.dataset.placed = "true";
+            piece.style.pointerEvents = "none";
+            piece.style.left = dropRect.left + "px";
+            piece.style.top = dropRect.top + "px";
+
+            placedCorrectly++;
+
+            if (placedCorrectly === 4) {
+                completePuzzle();
+            }
+        }
+    } else if (index == 1) {
+        if (
+            clickX >= dropRect.left + dropRect.width / 2 &&
+            clickX <= dropRect.left + dropRect.width &&
+            clickY >= dropRect.top &&
+            clickY <= dropRect.top + dropRect.height / 2
+        ) {
+            piece.dataset.placed = "true";
+            piece.style.pointerEvents = "none";
+            piece.style.left = dropRect.left + dropRect.width / 2 + "px";
+            piece.style.top = dropRect.top + "px";
+
+
+            placedCorrectly++;
+
+            if (placedCorrectly === 4) {
+                completePuzzle();
+            }
+        }
+    } else if (index == 2) {
+        if (dropRect.left <= clickX && clickX <= dropRect.left + (0.5 * dropRect.width) &&
+            (dropRect.top + (0.5 * dropRect.height)) <= clickY && clickY <= dropRect.top + dropRect.height) {
+            piece.dataset.placed = "true";
+            piece.style.pointerEvents = "none";
+            piece.style.left = dropRect.left + "px";
+            piece.style.top = dropRect.top + 0.35 * dropRect.height + "px";
+
+
+            placedCorrectly++;
+
+            if (placedCorrectly === 4) {
+                completePuzzle();
+            }
+        }
+    } else if (index == 3) {
+        if ((dropRect.left + (0.5 * dropRect.width)) <= clickX && clickX <= dropRect.left + dropRect.width &&
+            (dropRect.top + (0.5 * dropRect.height)) <= clickY && clickY <= dropRect.top + dropRect.height) {
+            piece.dataset.placed = "true";
+            piece.style.pointerEvents = "none";
+            piece.style.left = dropRect.left + 0.35 * dropRect.width + "px";
+            piece.style.top = dropRect.top + 0.5 * dropRect.height + "px";
+            placedCorrectly++;
+            if (placedCorrectly === 4) {
+                completePuzzle();
+            }
+        }
+    }
+}
+
+function completePuzzle() {
+
+    const title = document.getElementById("title");
+
+    document.querySelectorAll(".piece").forEach(p => {
+        p.style.opacity = "0";
+        p.style.transition = "opacity 0.6s ease";
+    });
+
+    dropZone.style.opacity = "0";
+    title.style.display = "none";
+
+    setTimeout(() => {
+
+        heroSection.style.display = "block";
+        whiteSection.style.display = "block";
+
+        // Enable scroll
+        // document.body.style.height = "300vh";
+
+        //ait a frame before initializing GSAP
+        requestAnimationFrame(() => {
+            initGSAP();
+            ScrollTrigger.refresh();
+        });
+
+    }, 700);
+}
+
+function initGSAP() {
+    const object = document.getElementById("monitor");
+    const background = document.querySelector(".final-image");
+
+    const tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".hero",
+            start: "top top",
+            end: "bottom top",
+            scrub: true,
+            pin: true
+        }
+    });
+
+    tl.to(background, { opacity: 0 }, 0);
+
+    tl.to(object, {
+        y: "10%",
+        scale: 2,
+    }, 0);
+
+    tl.add(() => {
+        object.src = "assets/flatline.gif";
+    }, 0.3);
+}
